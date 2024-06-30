b0653e662b5c045cad38d33b7f28a8719f737b45
rnxtenn <rnxart5798@gmail.com>
2024-06-30 13:04:26 +0530

    some pattern problems

---
+++ patterns.java
@@ -0,0 +1,15 @@
+package javaThings;
+
+public class patterns {
+    public static void main(String[] args) {
+        int n=5;
+        int m=4;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
